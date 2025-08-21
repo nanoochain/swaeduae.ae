6b4dead5ffe3fd30995e03cdf1f8cd804d6c25f0
Swaed UAE Deploy <ops@swaeduae.ae>
2025-08-21 07:38:10 +0000

    fix(seeder): remove stray backslash; make RegionSeeder syntactically valid

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -3,24 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class RegionSeeder extends Seeder
 {
     public function run(): void
     {
-        \$regions = [
-            'Abu Dhabi',
-            'Dubai',
-            'Sharjah',
-            'Ajman',
-            'Fujairah',
-            'Ras Al Khaimah',
-            'Umm Al Quwain'
-        ];
-
-        foreach (\$regions as \$region) {
-            DB::table('regions')->insertOrIgnore(['name' => \$region]);
-        }
+        // intentionally left empty; real region data seeded elsewhere
     }
 }
